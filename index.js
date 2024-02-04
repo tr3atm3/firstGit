08efb7648cb1653a9243cfec66c2954cf642ce05
tr3atm3 <tttushar.789@gmail.com>
2024-02-04 18:47:44 +0530

    assignment1

---
+++ index.js
@@ -0,0 +1,10 @@
+const mainHeading = document.getElementById("main-heading");
+const header = document.getElementById("header");
+const basketHeading = document.getElementById("basket-heading");
+const thanks = document.getElementById("thanks");
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color = "orange";
+header.style.backgroundColor = "green";
+header.style.borderBottom = "3px solid orange";
+basketHeading.style.color = "green";
+thanks.innerHTML = "<p>Please visit us again</p>";
