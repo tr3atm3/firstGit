a65360ee409c90bea1091550a8c64636974d8081
tr3atm3 <tttushar.789@gmail.com>
2024-02-04 18:50:52 +0530

    assignment4

--- index.js
+++ index.js
@@ -24,3 +24,36 @@ lists[4].style.color = "blue";
 for (let fruit of lists) {
   fruit.style.fontStyle = "italic";
 }
+
+// assignment4
+const mainHeading = document.querySelector("#main-heading");
+mainHeading.style.textAlign = "center";
+
+const fruits = document.querySelector(".fruits");
+fruits.style.backgroundColor = "gray";
+fruits.style.padding = "30px";
+fruits.style.margin = "30px";
+fruits.style.width = "50%";
+fruits.style.borderRadius = "5px";
+fruits.style.listStyleType = "none";
+
+const basketHeading = document.querySelector("h2");
+basketHeading.style.marginLeft = "30px";
+basketHeading.style.color = "brown";
+
+const fruitItems = document.querySelectorAll(".fruit");
+for (let fruit of fruitItems) {
+  fruit.style.backgroundColor = "white";
+  fruit.style.padding = "10px";
+  fruit.style.margin = "10px";
+  fruit.style.borderRadius = "5px";
+}
+const OddFruitItems = document.querySelectorAll(".fruit:nth-child(odd)");
+for (let fruit of OddFruitItems) {
+  fruit.style.backgroundColor = "lightgray";
+}
+const EvenFruitItems = document.querySelectorAll(".fruit:nth-child(even)");
+for (let fruit of EvenFruitItems) {
+  fruit.style.backgroundColor = "brown";
+  fruit.style.color = "white";
+}
