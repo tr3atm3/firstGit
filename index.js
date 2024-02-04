974ca63106fdd8f3358ac185f347408a5391f132
tr3atm3 <tttushar.789@gmail.com>
2024-02-04 18:48:54 +0530

    assignment2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// assignment1
+
 const mainHeading = document.getElementById("main-heading");
 const header = document.getElementById("header");
 const basketHeading = document.getElementById("basket-heading");
@@ -8,3 +10,10 @@ header.style.backgroundColor = "green";
 header.style.borderBottom = "3px solid orange";
 basketHeading.style.color = "green";
 thanks.innerHTML = "<p>Please visit us again</p>";
+
+// assignment2
+const fruits = document.getElementsByClassName("fruit");
+fruits[2].style.backgroundColor = "yellow";
+for (let fruit of fruits) {
+  fruit.style.fontWeight = "bold";
+}
